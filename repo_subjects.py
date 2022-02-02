875750eaf723fa5b08f21522c0816f50a1934d9a
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-02-02 14:58:14 +0100

    Get docs of manually assigned DDC subclasses

--- repo_subjects.py
+++ repo_subjects.py
@@ -53,6 +53,29 @@ def get_ddc():
   json.dump(sorted_ddc, open('data/json/dim/all/ddc_subjects.json', 'w'))
 
 
+def ddc_docs():
+  """ Once we have manually assigned DDC numbers to the OpenAlex fields, we now
+  retrieve the documents for each DDC number and store them by field. """
+  ddc = json.load(open('data/openalex/ddc_mapping.json'))
+  numbers = []
+  docs = {f: {} for f in ddc.keys()}
+  for field in ddc:
+    numbers += list(ddc[field])
+  folder = 'data/json/dim'
+  file = 'relevant_subjects_reversed.json'
+  for repo in ['depositonce', 'edoc', 'refubium']:
+    data = json.load(open(f'{folder}/{repo}/{file}'))
+    for subject in data:
+      number = subject['subject']
+      if subject['type'] == 'ddc' and number[:2] in numbers:
+        for field in ddc:
+          if number[:2] in ddc[field]:
+            if number in docs[field]:
+              docs[field][number] += subject['values']
+            else:
+              docs[field][number] = subject['values']
+  json.dump(docs, open('data/openalex/ddc_docs.json', 'w'))
+
+
 if __name__ == '__main__':
-  mapping_file = 'data/openalex/repo_mapping.json'
-  map_subjects(mapping_file)
+  ddc_docs()
