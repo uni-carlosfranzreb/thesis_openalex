41e50b6d41d4af57b89c5f1faae97caa912848af
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-02-02 14:57:51 +0100

    Dump numbers and doc counts of the DDC subjects

--- repo_subjects.py
+++ repo_subjects.py
@@ -2,6 +2,7 @@
 
 
 import json
+from copy import deepcopy
 
 
 def map_subjects():
@@ -28,6 +29,28 @@ def map_subjects():
           }
   json.dump(matches, open('data/openalex/repo_mapping.json', 'w'))
 
+
+def get_ddc():
+  """ Dump the numbers and doc counts of the DDC subjects. Sort them by the
+  number of documents they are assigned to. """
+  # dict of subjects (keys) and the list of publications they appear in (values).
+  folder = 'data/json/dim'
+  file = 'relevant_subjects_reversed.json'
+  depositonce = json.load(open(f'{folder}/depositonce/{file}'))
+  edoc = json.load(open(f'{folder}/edoc/{file}'))
+  refubium = json.load(open(f'{folder}/refubium/{file}'))
+  all = {(s['subject'], s['type']): deepcopy(s['values']) for s in depositonce}
+  for repo in (edoc, refubium):
+    for s in repo:
+      if (s['subject'], s['type']) in all:
+        all[(s['subject'], s['type'])] += s['values']
+      else:
+        all[(s['subject'], s['type'])] = s['values']
+  ddc = {k[0]: len(v) for k, v in all.items() if k[1] == 'ddc'}
+  sorted_ddc = {
+    k: v for k, v in sorted(ddc.items(), key=lambda t: t[1], reverse=True)
+  }
+  json.dump(sorted_ddc, open('data/json/dim/all/ddc_subjects.json', 'w'))
 
 
 if __name__ == '__main__':
